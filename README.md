562806c482e77b020866f03c8dd2e8fde54e90c3
Carl Barrdahl <carlbarrdahl@gmail.com>
2023-08-19 13:32:30 +0530

    Setup Next.js with Tailwind

---
+++ README.md
@@ -0,0 +1,1 @@
+# Optimism Retro PGF 3
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Retro PGF</title>
+      </Head>
+      <main></main>
+    </>
+  );
+}
