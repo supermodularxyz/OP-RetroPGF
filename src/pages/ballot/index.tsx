71e62032b2069be3553be988aa11a02384146f4a
Carl Barrdahl <carlbarrdahl@gmail.com>
2023-08-30 16:41:27 +0530

    Add ballot empty state

--- src/pages/ballot/index.tsx
+++ src/pages/ballot/index.tsx
@@ -1,9 +1,11 @@
+import Link from "next/link";
 import { useState } from "react";
 import { useFormContext } from "react-hook-form";
 import { type z } from "zod";
 import { AllocationForm, AllocationSchema } from "~/components/AllocationList";
 import { Layout } from "~/components/Layout";
 import { SortBy } from "~/components/SortBy";
+import { Button } from "~/components/ui/Button";
 import { Form, SearchInput } from "~/components/ui/Form";
 import { arrayToBallot } from "~/hooks/useBallot";
 import {
@@ -62,7 +64,13 @@ export default function BallotPage() {
                 />
                 <SortBy value={sort} onChange={setSort} options={options} />
               </div>
-              <AllocationForm filter={filter} onSave={handleSaveBallot} />
+              <div className="relative flex min-h-[360px] flex-col">
+                {allocations.length ? (
+                  <AllocationForm filter={filter} onSave={handleSaveBallot} />
+                ) : (
+                  <EmptyBallot />
+                )}
+              </div>
             </div>
             <div className="flex justify-between rounded-b-2xl border-t border-gray-300 bg-[#EDF4FC] px-8 py-4 text-lg font-semibold">
               <div>Total OP in ballot</div>
@@ -75,6 +83,27 @@ export default function BallotPage() {
     </Layout>
   );
 }
+
+const EmptyBallot = () => (
+  <div className="flex flex-1 items-center justify-center">
+    <div className=" max-w-[360px] space-y-4">
+      <h3 className="text-center text-lg font-bold">Your ballot is empty</h3>
+      <p className="text-center text-sm text-gray-700">
+        Your ballot currently doesn&apos;t have any projects added. Browse
+        through the available projects and lists.
+      </p>
+      <div className="flex items-center justify-center gap-3">
+        <Button variant="outline" as={Link} href={"/projects"}>
+          View projects
+        </Button>
+        <div className="text-gray-700">or</div>
+        <Button variant="outline" as={Link} href={"/lists"}>
+          View lists
+        </Button>
+      </div>
+    </div>
+  </div>
+);
 
 const TotalOP = () => {
   const form = useFormContext();
