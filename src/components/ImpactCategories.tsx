916209e57ade49244a6d35dd982aae37e88ba8e9
Carl Barrdahl <carlbarrdahl@gmail.com>
2023-09-10 11:02:25 +0800

    Scrollable impact categories in ProjectCard

--- src/components/ImpactCategories.tsx
+++ src/components/ImpactCategories.tsx
@@ -5,11 +5,13 @@ import {
 import { Tag } from "./ui/Tag";
 
 export const ImpactCategories = ({ tags }: { tags?: ImpactCategory[] }) => (
-  <div className="flex gap-1">
-    {tags?.map((key) => (
-      <Tag key={key} size="sm">
-        {impactCategoryLabels[key]}
-      </Tag>
-    ))}
+  <div className="no-scrollbar ">
+    <div className="flex gap-1 overflow-x-auto">
+      {tags?.map((key) => (
+        <Tag key={key} size="sm">
+          {impactCategoryLabels[key]}
+        </Tag>
+      ))}
+    </div>
   </div>
 );
