d1df2a03bc5a8f9b06d341dd93b27136d41742e8
Carl Barrdahl <carlbarrdahl@gmail.com>
2023-09-12 09:13:39 +0800

    Fix right sidebar

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -22,7 +22,9 @@ export const Layout = (
   if (!isLoaded) return null;
 
   const sidebar = (
-    <Sidebar>{address ? <BallotOverview /> : <SunnyBanner />}</Sidebar>
+    <Sidebar side={props.sidebar}>
+      {address ? <BallotOverview /> : <SunnyBanner />}
+    </Sidebar>
   );
   return (
     <>
@@ -32,25 +34,35 @@ export const Layout = (
 
       <main className="text-gray-900">
         <Header />
-        <div className="container mx-auto max-w-screen-2xl gap-8 pt-12 md:flex">
-          {props.sidebar === "left" ? <Sidebar>{sidebar}</Sidebar> : null}
+        <div className="container mx-auto max-w-screen-2xl pt-12 md:flex">
+          {props.sidebar === "left" ? sidebar : null}
           <div
             className={clsx("min-w-0 flex-1 px-4 pb-24", {
               ["mx-auto max-w-5xl"]: !props.sidebar,
-              ["md:ml-8 md:pl-80"]: props.sidebar,
+              ["md:ml-8 md:pl-80"]: props.sidebar === "left",
+              ["md:mr-8 md:pr-80"]: props.sidebar === "right",
             })}
           >
             {props.children}
           </div>
-          {props.sidebar === "right" ? <Sidebar>{sidebar}</Sidebar> : null}
+          {props.sidebar === "right" ? sidebar : null}
         </div>
       </main>
     </>
   );
 };
 
-const Sidebar = (props: PropsWithChildren) => (
+const Sidebar = ({
+  side,
+  ...props
+}: { side: "left" | "right" } & PropsWithChildren) => (
   <div className="">
-    <div className="w-[336px] px-2 md:fixed md:px-4" {...props} />
+    <div
+      className={clsx("w-[336px] px-2 md:fixed md:px-4", {
+        ["left-2"]: side === "left",
+        ["right-2"]: side === "right",
+      })}
+      {...props}
+    />
   </div>
 );
