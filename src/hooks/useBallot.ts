e21f70d4cf1c290cfc1c11b3de4e7103a66a14fc
Carl Barrdahl <carlbarrdahl@gmail.com>
2023-09-24 12:22:50 +0800

    Add backend call when saving ballot #71

--- src/hooks/useBallot.ts
+++ src/hooks/useBallot.ts
@@ -36,9 +36,15 @@ export function useBallot() {
 
 export function useSaveBallot() {
   const queryClient = useQueryClient();
-  return useMutation(async (ballot: Ballot) =>
-    queryClient.setQueryData(["ballot"], ballot)
-  );
+  const { address } = useAccount();
+
+  return useMutation(async (ballot: Ballot) => {
+    queryClient.setQueryData(["ballot"], ballot);
+    return axios.post(`${backendUrl}/api/ballot/save`, {
+      address,
+      votes: mapBallotForBackend(ballot),
+    });
+  });
 }
 
 const backendUrl = process.env.NEXT_PUBLIC_BACKEND_API!;
@@ -59,10 +65,7 @@ export function useSubmitBallot({
         .post(`${backendUrl}/api/ballot/submit`, {
           address,
           signature,
-          votes: ballotToArray(ballot).map((p) => ({
-            projectId: p.id,
-            amount: p.amount,
-          })),
+          votes: mapBallotForBackend(ballot),
         })
         .then(onSuccess)
     );
@@ -111,3 +114,10 @@ export const sumBallot = (allocations: Allocation[] = []) =>
   allocations.reduce((sum, x) => sum + (x?.amount ?? 0), 0);
 
 export const countBallot = (ballot: Ballot = {}) => Object.keys(ballot).length;
+
+function mapBallotForBackend(ballot?: Ballot) {
+  return ballotToArray(ballot).map((p) => ({
+    projectId: p.id,
+    amount: p.amount,
+  }));
+}
