a1d80f44e0e8634204c85fa3af4847bcc69b33fa
Carl Barrdahl <carlbarrdahl@gmail.com>
2023-10-27 17:33:37 +0200

    Add ipfs gateway to created list metadata

--- src/utils/ipfs.ts
+++ src/utils/ipfs.ts
@@ -8,15 +8,7 @@ export const uploadIpfs = async (data: object, fileName = "metadata.json") => {
   // Data can be either File or a js object
   const blob = new Blob([JSON.stringify(data)], { type: "application/json" });
   const file = new File([blob], fileName);
-  return ipfsClient.put([file]).then((cid: string) => `${cid}/${file.name}`);
-};
-
-export const fetchIpfs = async (cid?: string) => {
-  if (!cid) return null;
-  const ipfsGateway =
-    process.env.NEXT_PUBLIC_IPFS_GATEWAY ?? "https://dweb.link/ipfs/";
-
-  return fetch(`${ipfsGateway}${cid}`, {
-    headers: { "content-type": "application/json" },
-  }).then((r) => (r.ok ? r.json() : r));
+  return ipfsClient
+    .put([file])
+    .then((cid: string) => `https://w3s.link/ipfs/${cid}/${file.name}`);
 };
