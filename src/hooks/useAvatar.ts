b22e4089b00fca2264383afafea58ade9e6d716b
Carl Barrdahl <carlbarrdahl@gmail.com>
2023-10-31 17:05:52 +0100

    Fix build

--- src/hooks/useAvatar.ts
+++ src/hooks/useAvatar.ts
@@ -6,8 +6,8 @@ export function useAvatar(address?: Address) {
   const ens = useEnsName({ address, chainId: 1, enabled: Boolean(address) });
   const name = ens.data ?? truncate(address);
   const avatar = useEnsAvatar({ name, chainId: 1, enabled: Boolean(name) });
-  const error = ens.error || avatar.error;
-  const isLoading = ens.isLoading || avatar.isLoading;
+  const error = ens.error ?? avatar.error;
+  const isLoading = ens.isLoading ?? avatar.isLoading;
   return {
     ...ens,
     error,
