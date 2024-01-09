3d74c60f457fc927bf7bdb606a8c3dfe3bbba03e
Carl Barrdahl <carlbarrdahl@gmail.com>
2024-01-09 07:02:17 +0100

    Add footer with link to EasyRPGF

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,6 +7,8 @@ import { SunnyBanner } from "./SunnyBanner";
 import { Header } from "./Header";
 import { BallotOverview } from "./BallotOverview";
 import { useRouter } from "next/router";
+import { Code } from "./icons";
+import { FaCode, FaGithub } from "react-icons/fa6";
 
 const VOTING_END_DATE =
   process.env.NEXT_PUBLIC_VOTING_END_DATE ??
@@ -74,9 +76,9 @@ export const Layout = (
         />
       </Head>
 
-      <main className="text-gray-900">
+      <main className="flex h-full flex-1 flex-col text-gray-900">
         <Header />
-        <div className="mx-auto pt-12  2xl:container lg:flex">
+        <div className="mx-auto h-full w-full flex-1 pt-12 2xl:container lg:flex">
           {props.sidebar === "left" ? sidebar : null}
           <div
             className={clsx("min-w-0 flex-1 px-4 pb-24", {
@@ -87,6 +89,26 @@ export const Layout = (
           </div>
           {props.sidebar === "right" ? sidebar : null}
         </div>
+        <footer className="flex flex-col items-center justify-center bg-gray-950 p-2 text-gray-400">
+          <a
+            href={"https://github.com/gitcoinco/easy-retro-pgf/"}
+            target="_blank"
+            className="group py-4 text-sm hover:text-white"
+          >
+            <div className="flex">
+              Run with{" "}
+              <span className="relative -mt-1 w-6 px-1 text-xl text-primary-600">
+                <span className="absolute">❤️</span>
+                <span className="absolute group-hover:animate-ping">❤️</span>
+              </span>
+              by EasyRetroPGF.
+            </div>
+            <div className="inline-flex">
+              Run your own RPGF Round
+              <FaGithub className="ml-1 mt-0.5 h-4 w-4" />
+            </div>
+          </a>
+        </footer>
       </main>
     </>
   );
